dd4438942279f0b3f5575a352ffbbb7fa4517469
mwestacott <mwestacott1@companieshouse.gov.uk>
2020-01-17 10:32:42 +0000

    LFA-1196 Improved assertions in testing to address review comments

--- test/routes/index.spec.unit.ts
+++ test/routes/index.spec.unit.ts
@@ -6,19 +6,24 @@ describe("Basic URL Tests", () => {
   it("should find start page", async () => {
     const response = await request(app)
       .get("/promise-to-file");
+
     expect(response.status).toEqual(200);
+    expect(response.text).toMatch(/Use this service to stop us from removing a company/);
   });
 
   it("should find company number page", async () => {
     const response = await request(app)
       .get("/promise-to-file/company-number")
-      .set("Referer", "/")
-     expect(response.status).toEqual(200);
+
+    expect(response.status).toEqual(200);
+    expect(response.text).toMatch(/What is the company number/);
   });
 
   it("should return 404 if page doesn't exist", async() => {
     const response = await request(app)
       .get("/gibberish");
+
     expect(response.status).toEqual(404);
+    expect(response.text).toMatch(/Sorry, there is a problem with the service/);
   });
 });
