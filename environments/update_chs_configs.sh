6526a2745c8a5d12ce8d54ff781f7093206d2b9f
Matthew Culpin <mculpin@companieshouse.gov.uk>
2020-01-17 13:28:31 +0000

    As requested added comment on the script's usage

--- environments/update_chs_configs.sh
+++ environments/update_chs_configs.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# The script updates the chs configs environments for promise-to-file-web,
+# the idea is to avoid manually having to go to each environment and update, instead we can update from a standard template.
+# An argument is passed in "dev", "live" or "all", further custom lists can be added.
+
+# A list is selected from the releveant argument file "dev" etc and added to a path for the environments selected,
+# the promise-to-file-web config is then updated in that enviroment using the template_env file which overwrites the content if the file exists.
+# If the file does not exist it creates the file with the content.
+
+# The user types in the shell file name with sh on the command line together with an argument "dev", "live" or "all".
+# The user is then prompted to ensure that they have checked out a branch in chs configs
+# and to check that they have correctly selected dev/live/all.
+
 FILE="./env_template"
 CONFIG_DIR="../../chs-configs"
 APP_DIR="promise-to-file-web"
