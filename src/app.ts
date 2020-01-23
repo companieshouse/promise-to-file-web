7052f4adec011f3ca1728f94fc441b6f93624012
mwestacott <mwestacott1@companieshouse.gov.uk>
2020-01-23 15:09:25 +0000

    LFA-1222 Implement sign-in when user not authenticated

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import * as express from "express";
 import * as nunjucks from "nunjucks";
 import * as path from "path";
 import * as createError from "http-errors";
-import * as logger from "morgan";
+import * as morganLogger from "morgan";
 
 import {appRouter} from "./routes/routes";
 import * as pageURLs from "./model/page.urls";
 import sessionMiddleware from "./session/middleware";
+import authenticate from "./authentication/middleware/index";
+import logger from "./logger";
 
 const app = express();
 
@@ -21,30 +23,29 @@ const env = nunjucks.configure([
   express: app,
 });
 
-// TODO Configure the view engine for CHS - uncomment the following lines and change as appropriate
-
 env.addGlobal("CDN_URL", process.env.CDN_HOST);
+
+// TODO Configure PIWIK monitoring - uncomment the following lines and change as appropriate
 
 // env.addGlobal("ERROR_SUMMARY_TITLE", ERROR_SUMMARY_TITLE);
 // env.addGlobal("PIWIK_URL", PIWIK_URL);
 // env.addGlobal("PIWIK_SITE_ID", PIWIK_SITE_ID);
 
-// app.enable("trust proxy");
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(sessionMiddleware);
-// app.use(`${pageURLs.EXTENSIONS}/*`, authenticate);
+app.enable("trust proxy");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(sessionMiddleware);
+
+app.use(`${pageURLs.PROMISE_TO_FILE}/*`, authenticate);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "html");
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(sessionMiddleware);
+app.use(morganLogger("dev"));
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(pageURLs.PROMISE_TO_FILE, appRouter);
@@ -56,6 +57,9 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res, next) => {
+
+  logger.error("An error has occured. Re-routing to the error screen - " + err.stack);
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
