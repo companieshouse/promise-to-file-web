7093b5b1d224e58c9c201526e672d3618c50d62c
Svarallo <Svarallo@companieshouse.gov.uk>
2020-01-30 15:18:00 +0000

    Updated parameter names

--- src/controllers/check.company.controller.ts
+++ src/controllers/check.company.controller.ts
@@ -4,9 +4,9 @@ import * as templatePaths from "../model/template.paths";
 export const route = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 
   const scenario = { company: { PTFRequested: "Yes", accountsDue: "01 January 2020",
-    accountsStatus: "overdue", address: "25 No Street, Nowhere, nl2br", companyType: "Private Limited Company",
+    accountsStatus: "overdue", address: "25 No Street, Nowhere, nl2br", type: "Private Limited Company",
     csDue: "02 January 2020", csStatus: "overdue", incorporationDate: "01 January 2000",
-    name: "Test Company LTD", companyNumber: "00001111", companyStatus: "Active" }};
+    name: "Test Company LTD", number: "00001111", status: "Active" }};
 
   return res.render(templatePaths.CHECK_COMPANY, {
     scenario,
