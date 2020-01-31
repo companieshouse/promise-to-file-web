8bbab6c9ca3c5c0f6f7d848923254601885e473c
Svarallo <Svarallo@companieshouse.gov.uk>
2020-01-31 15:34:11 +0000

    Indentation

--- src/controllers/check.company.controller.ts
+++ src/controllers/check.company.controller.ts
@@ -5,10 +5,10 @@ import { PTFCompanyProfile } from "../model/company.profile";
 export const route = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 
   const company: PTFCompanyProfile = { accountingPeriodEndOn: "", accountingPeriodStartOn: "",
-  accountsDue: "01 January 2020", address: { line_1: "25 No Street", line_2: "Nowhere", postCode: "nl2br" },
-  companyName: "Test Company LTD", companyNumber: "00001111", companyStatus: "Active",
-  companyType: "Private Limited Company", confirmationStatementDue: "02 January 2020",
-  incorporationDate: "01 January 2000", isAccountsOverdue: true, isConfirmationStatementOverdue: true };
+    accountsDue: "01 January 2020", address: { line_1: "25 No Street", line_2: "Nowhere", postCode: "nl2br" },
+    companyName: "Test Company LTD", companyNumber: "00001111", companyStatus: "Active",
+    companyType: "Private Limited Company", confirmationStatementDue: "02 January 2020",
+    incorporationDate: "01 January 2000", isAccountsOverdue: true, isConfirmationStatementOverdue: true };
 
   return res.render(templatePaths.CHECK_COMPANY, {
     company,
