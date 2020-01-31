3da563b6fad1c39667bc3ea10b2d87a90cc603cd
markpit <mpitten@companieshouse.gov.uk>
2020-01-31 10:22:47 +0000

    Adding comments for page urls

--- src/model/page.urls.ts
+++ src/model/page.urls.ts
@@ -2,6 +2,8 @@ import * as TEMPLATE from "./template.paths";
 
 /**
  * Keep template names in template.paths.ts and reference them in here for URLs
+ * The app is set up to use pageURLs.PROMISE_TO_FILE as the base url so all urls in here
+ * will be automatically applied to the end of the base url when forwarding/redirecting.
  */
 
 const SEPARATOR: string = "/";
