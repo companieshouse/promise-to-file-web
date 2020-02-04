c8d6914208b55b9b24f73f01e7f11cb2680d5b38
markpit <mpitten@companieshouse.gov.uk>
2020-02-04 13:07:41 +0000

    Adding comments to redis.store.mock.factory

--- test/mocks/redis.store.mock.factory.ts
+++ test/mocks/redis.store.mock.factory.ts
@@ -1,3 +1,9 @@
+/**
+ * This factory will override the redis store constructor and prevent a redis connection attempt
+ * To use this in the tests:
+ * jest.mock("../../src/session/store/redis.store", () => import("../mocks/redis.store.mock.factory"));
+ * Paths may be different depending on location
+ */
 export default () => {
   return {
     default: {},
