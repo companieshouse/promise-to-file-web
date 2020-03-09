2906879dacd9dad837a8fccdbcaf4625fd07eed0
Svarallo <Svarallo@companieshouse.gov.uk>
2020-03-09 11:51:35 +0000

    Formatting

--- src/authentication/user/middleware/index.ts
+++ src/authentication/user/middleware/index.ts
@@ -5,7 +5,7 @@ import * as pageURLs from "../../../model/page.urls";
 export default (req: Request, res: Response, next: NextFunction) => {
   const referringPageURL = req.header("Referer") as string;
 
-  logger.debug("Check if user has referrer ");
+  logger.debug("Check if user has referrer");
   if (referringPageURL === undefined) {
     logger.debug("User has no referrer - redirecting to index");
     return res.redirect(pageURLs.PROMISE_TO_FILE);
