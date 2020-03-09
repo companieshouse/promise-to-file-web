8c7ac7a3ed0819bee0ed01f56c13332faf2654be
Svarallo <Svarallo@companieshouse.gov.uk>
2020-03-09 11:19:25 +0000

    Updated unit tests to increase coverage

--- src/authentication/user/middleware/index.ts
+++ src/authentication/user/middleware/index.ts
@@ -5,27 +5,25 @@ import * as pageURLs from "../../../model/page.urls";
 export default (req: Request, res: Response, next: NextFunction) => {
   const referringPageURL = req.header("Referer") as string;
 
-  logger.debug("Check if user has referrer");
-
+  logger.debug("Check if user has referrer ");
   if (referringPageURL === undefined) {
-
     logger.debug("User has no referrer - redirecting to index");
     return res.redirect(pageURLs.PROMISE_TO_FILE);
   }
 
-  logger.debug("Check if user is authenticated");
-
+  logger.debug("Check if user is signed in");
   if (!req.chSession.isSignedIn()) {
 
-    logger.debug("User not authenticated");
+    logger.debug("User not signed in");
 
     let returnToUrl: string = pageURLs.PROMISE_TO_FILE;
 
+    // if user is coming from start page
     if (referringPageURL.endsWith(pageURLs.PROMISE_TO_FILE)) {
       returnToUrl = req.originalUrl;
     }
 
-    logger.debug("User not authenticated - redirecting to login screen");
+    logger.debug("User not signed in - redirecting to login screen");
     return res.redirect("/signin?return_to=" + returnToUrl);
   }
 
--- test/authentication/user/middleware/index.spec.unit.ts
+++ test/authentication/user/middleware/index.spec.unit.ts
@@ -1,3 +1,4 @@
+import { json } from "express";
 import * as request from "supertest";
 import app from "../../../../src/app";
 import { COOKIE_NAME } from "../../../../src/properties";
@@ -10,6 +11,9 @@ jest.mock("../../../../src/session/store/redis.store",
   () => import("../../../mocks/redis.store.mock.factory"));
 jest.mock("../../../../src/services/redis.service");
 
+const INDEX_STRING: string = "Use this service to tell us if you still require a company that has overdue filing.";
+const COMPANY_NUMBER_STRING: string = "What is the company number?";
+
 const mockCacheService = loadSession as jest.Mock;
 
 beforeEach(() => {
@@ -19,32 +23,55 @@ beforeEach(() => {
 
 describe("Authentication middleware", () => {
 
-  it("should not redirect to signin if loading start page", async () => {
+  it("should load start page with no referrer and not signed in", async () => {
+    setNotSignedIn();
     const response = await request(app)
       .get("/promise-to-file");
     expect(response.status).toEqual(200);
+    expect(response.text).toContain(INDEX_STRING);
   });
 
-  it("should redirect to start page if loading start page with trailing slash", async () => {
+  it("should load start page if loading start page with trailing slash", async () => {
     const response = await request(app)
-      .get("/promise-to-file/");
-    expect(response.status).toEqual(302);
+      .get("/promise-to-file/")
+      .set("Referer", "/")
+      .set("Cookie", [`${COOKIE_NAME}=123`]);
+    expect(response.status).toEqual(200);
+    expect(response.text).toContain(INDEX_STRING);
   });
 
-  it("should redirect to start page if /promise-to-file/* called and signed in", async () => {
+  it("should redirect to start page if loading start page with trailing slash and no referrer", async () => {
     const response = await request(app)
-      .get("/promise-to-file/company-number")
+      .get("/promise-to-file/")
+      .set("Cookie", [`${COOKIE_NAME}=123`])
       .expect("Location", "/promise-to-file");
     expect(response.status).toEqual(302);
+  });
+
+  it("should not redirect to start page if user has referrer and is signed in", async () => {
+    const response = await request(app)
+      .get("/promise-to-file/company-number")
+      .set("Referer", "/promise-to-file/")
+      .set("Cookie", [`${COOKIE_NAME}=123`]);
+    expect(response.status).toEqual(200);
+    expect(response.text).toContain(COMPANY_NUMBER_STRING);
   });
 
   it("should redirect to signin if /promise-to-file/* called and not signed in", async () => {
     setNotSignedIn();
     const response = await request(app)
       .get("/promise-to-file/company-number")
-      .set("Referer", "/promise-to-file/company-number")
+      .set("Referer", "/promise-to-file/")
       .expect("Location", "/signin?return_to=/promise-to-file");
     expect(response.status).toEqual(302);
+  });
+
+  it("should not redirect to signin if loading start page and not signed in", async () => {
+    setNotSignedIn();
+    const response = await request(app)
+      .get("/promise-to-file")
+      .set("Referer", "/");
+    expect(response.status).toEqual(200);
   });
 
   it("should redirect to signin if navigating from /promise-to-file page and not signed in", async () => {
@@ -59,17 +86,18 @@ describe("Authentication middleware", () => {
   it("should not redirect to signin if /promise-to-file/* called while signed in", async () => {
     const response = await request(app)
       .get("/promise-to-file/company-number")
-      .set("Referer", "/")
+      .set("Referer", "/promise-to-file")
       .set("Cookie", [`${COOKIE_NAME}=123`]);
     expect(response.status).toEqual(200);
   });
 
-  it("should redirect to start screen if /promise-to-file/* called from outside of the PTF journey and not signed in",
+  it("should redirect to sign in screen if /promise-to-file/* called from outside of the PTF journey and not signed in",
       async () => {
     setNotSignedIn();
     const response = await request(app)
       .get("/promise-to-file/company-number")
-      .expect("Location", "/promise-to-file");
+      .set("Referer", "/")
+      .expect("Location", "/signin?return_to=/promise-to-file");
     expect(response.status).toEqual(302);
   });
 });
