45018312b2fd70be00908e9ef87f451823ab56f6
ICoxCH <ICox@companieshouse.gov.uk>
2020-04-21 13:42:51 +0100

    Change not in prosecution to no open compliance

--- src/model/eligibilityReasonCode.ts
+++ src/model/eligibilityReasonCode.ts
@@ -1,6 +1,6 @@
 export enum eligibilityReasonCode {
   COMPANY_IS_ELIGIBLE = "company is eligible",
-  NOT_IN_PROSECUTION = "no open compliance case",
+  NO_OPEN_COMPLIANCE_CASE = "no open compliance case",
   PERSISTENTLY_LATE = "history of late filings",
   EXISTING_PTF = "existing promise to file",
   NO_DIRECTORS = "no appointed directors",
