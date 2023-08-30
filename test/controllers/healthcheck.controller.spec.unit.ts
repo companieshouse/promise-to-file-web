50ce0da26c6a3fc139858f0053beadaf0ad3f01c
wdhda-ch <awoodhead@companieshouse.gov.uk>
2023-08-30 15:17:26 +0100

    Change to make test logs have less error logs

--- test/controllers/healthcheck.controller.spec.unit.ts
+++ test/controllers/healthcheck.controller.spec.unit.ts
@@ -3,7 +3,7 @@ import app from "../../src/app";
 import { HEALTHCHECK } from "../../src/model/page.urls";
 import { loadSession } from "../../src/services/redis.service";
 
-
+jest.mock("../../src/session/store/redis.store", () => import("../mocks/redis.store.mock.factory"));
 jest.mock("../../src/services/redis.service");
 const mockCacheService = loadSession as jest.Mock;
 
